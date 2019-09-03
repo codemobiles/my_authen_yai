a821722e548c9aef859c3aa8f9552157bb289045
iBlurBlur <tanakorn.ngam@gmail.com>
2019-09-03 15:57:25 +0700

    .....

--- app/src/main/java/com/codemobiles/myauthen/JSONFragment.kt
+++ app/src/main/java/com/codemobiles/myauthen/JSONFragment.kt
@@ -1,18 +1,52 @@
 package com.codemobiles.myauthen
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_json.view.*
 
 class JSONFragment : Fragment() {
+
+    private lateinit var mAdapter: CustomAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_json, container, false)
+        val _view = inflater.inflate(R.layout.fragment_json, container, false)
+
+        _view.recyclerView.adapter = mAdapter
+        return _view
     }
+
+
+    class CustomAdapter(val context: Context) :
+        RecyclerView.Adapter<CustomViewHolder>() {
+
+        override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int = 100
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+            return CustomViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.custom_list,
+                    parent,
+                    false
+                )
+            )
+        }
+    }
+
+    class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
 }
